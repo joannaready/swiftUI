76d49b754b2b67ef24efd1e6181722d968b1b545
乔星星 <joannaready123@gmail.com>
2015-03-25 00:25:40 +0800

    init 1. 加法计算器

---
+++ somePractices/somePractices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  somePractices
+//
+//  Created by qiao xingxing on 15/3/25.
+//  Copyright (c) 2015年 qiao xingxing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
